bc6bd1f3ad44fab615d566746345646edb981663
Vasyl Hevko <hevko.vasyl.me@gmail.com>
2021-02-18 01:13:22 +0200

    123

--- src/index.js
+++ src/index.js
@@ -1,1 +1,49 @@
 import './styles.css';
+import refs from './js/refs.js';
+import galleryHbs from './templates/gallery.hbs';
+import apiSevice from './js/apiService';
+// import observer from './js/observer';
+
+
+console.log(refs.form);
+
+refs.form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const input = event.currentTarget;
+    apiSevice.searchQuery = input.elements.query.value;
+    input.reset();
+    apiSevice.resetPage();
+    clearMarkup();
+    console.log(apiSevice.searchQuery);
+
+    apiSevice.fetchImages().then(hits => makeImageMarkup(hits))
+        .then(observer.observe(refs.gallery));
+
+
+    // observer.observe(refs.sentinel)
+    // observer().observe(refs.sentinel)
+});
+
+
+
+
+    const observer = new IntersectionObserver(entries => {
+    if (!entries.some(entry => entry.intersectionRatio > 0)) {
+        return
+    }
+    apiSevice.fetchImages().then(hits => makeImageMarkup(hits));
+    })
+
+
+
+
+
+function makeImageMarkup(images) {
+    const markup = galleryHbs(images);
+    refs.gallery.insertAdjacentHTML('beforeend', markup);
+};
+
+
+function clearMarkup() {
+    refs.gallery.innerHTML = '';
+};
